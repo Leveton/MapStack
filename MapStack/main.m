6e70db8a641605ec402581c556c4695e69567d77
Leveton <mleveton@gmail.com>
2016-02-23 22:54:12 -0500

    Reference vs value types

--- MapStack/main.m
+++ MapStack/main.m
@@ -9,8 +9,37 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+struct vanillaStruct {
+    int anInteger;
+    char *firstName;
+};
+
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+    
+    struct vanillaStruct valueType0;
+    valueType0.anInteger = 7;
+    valueType0.firstName = "mike";
+    
+    NSObject *objectType0 = [[NSObject alloc] init];
+    [objectType0 setAccessibilityLabel:@"I'm a string"];
+    
+    struct vanillaStruct valueType1 = valueType0;
+    NSObject *objectType1           = objectType0;
+    
+    printf("first names: %s, %s \n", valueType0.firstName, valueType1.firstName);
+    printf("anIntegers: %d, %d \n", valueType0.anInteger, valueType1.anInteger);
+    NSLog(@"accessibiltyLabels: %@ %@", objectType0.accessibilityLabel, objectType1.accessibilityLabel);
+    
+    valueType0.firstName = "mary";
+    [objectType0 setAccessibilityLabel:@"I'm a different string"];
+    
+    printf("first names again: %s, %s \n", valueType0.firstName, valueType1.firstName);
+    printf("anIntegers again: %d, %d \n", valueType0.anInteger, valueType1.anInteger);
+    NSLog(@"accessibiltyLabels again: %@ %@", objectType0.accessibilityLabel, objectType1.accessibilityLabel);
+    
+    
+    
+    //    @autoreleasepool {
+    //        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    //    }
 }
